2d5c1b4748df09a11bee5ad801861ccf6adfadea
zankerion <zankerion@gmail.com>
2023-09-18 16:12:41 +0300

    first button with the text change

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            // Change the TextView text when the button is clicked
+            textView.text = "sefuiafseaefwfwae"
+        }
     }
 }
